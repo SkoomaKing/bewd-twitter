e7b28aff531f9ec7ef066bff09213a86c37a6d32
Caleb Burrus <calebburrus@gmail.com>
2024-01-10 20:55:06 +0000

    users controller passing rspec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render 'users/create'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email)
+  end
 end
