98a1e9f44b7b1c99185f27ded0b14aeb3b1ce6f5
Caleb Burrus <calebburrus@gmail.com>
2024-01-10 19:26:58 +0000

    has_many plural formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 8, maximum: 64 }
   validates :email, presence: true, length: { minimum: 5, maximum: 500 }
 
-  has_many :sessions, :tweets
+  has_many :sessions
+  has_many :tweets
 end
