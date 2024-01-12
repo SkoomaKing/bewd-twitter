9683295542806f3c83915c5c67333f0be9b25371
Caleb Burrus <calebburrus@gmail.com>
2024-01-12 20:30:41 +0000

    tweets controller post new tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,25 @@
 class TweetsController < ApplicationController
+  def create
+    token = cookies.signed[:twitter_session_token]
+    session = Session.find_by(token: token)
+
+    if session
+      user = session.user
+      @tweet = user.tweets.new(tweet_params)
+      if @tweet.save
+        render json: {
+          tweet: {
+            username: user.username,
+            message: @tweet.message
+          }
+        }
+      end
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:message)
+  end
 end
